82ce98a9f98fdf83d04160abeb483db35eeae9df
Abhinav Chemikala <50442554+abhiboby@users.noreply.github.com>
2022-05-25 01:16:26 +0530

    Obstacle Death

---
+++ Assets/Scripts/ObstacleDeath.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleDeath : MonoBehaviour
+{
+    public GameObject LevelMusic;
+    public void OnTriggerEnter(Collider Col)
+    {
+        if (Col.gameObject.tag == "Player" || Col.gameObject.tag == "Angel" || Col.gameObject.tag == "Theif" || Col.gameObject.tag == "Runner")
+        {
+            Destroy(LevelMusic);
+            Time.timeScale = 0;
+            Destroy(Col.gameObject);
+        }
+    }
+}

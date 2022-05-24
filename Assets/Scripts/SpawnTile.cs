fde2744a72938d0e66f7650204dfa7ce2c46bf5f
Abhinav Chemikala <50442554+abhiboby@users.noreply.github.com>
2022-05-25 01:43:07 +0530

    Initial Obstacle Generation

--- Assets/Scripts/SpawnTile.cs
+++ Assets/Scripts/SpawnTile.cs
@@ -12,15 +12,20 @@ public class SpawnTile : MonoBehaviour
 
     public GameObject deathTriggerToSpawn;
     public GameObject deathTriggerReference;
+
+    public GameObject obstacleToSpawn;
+    public GameObject obstaclereference;
     
     public float timeOffset = 0.4f;
-    public float timeOffsetCoin = 0.4f;
+    public float timeOffsetCoin = 0.8f;
     public float distanceBetweenTiles = 5.0F;
     public float distanceBetweenCoin = 5.0F;
+    public float distanceBetweenObstacle = 5.0f;
 
     public float randomValue = 0.8f;
     private Vector3 previousTilePosition;
     private Vector3 previousCoinPosition;
+    private Vector3 previousObstaclePosition;
     private Vector3 previosDeathTriggerPosition;
     private float startTime;
     private Vector3 direction, mainDirection = new Vector3(0, 0, 1), otherDirection = new Vector3(1, 0, 0);
@@ -31,8 +36,11 @@ public class SpawnTile : MonoBehaviour
         previousTilePosition = referenceObject.transform.position;
         previousCoinPosition = referenceObject.transform.position;
         previosDeathTriggerPosition = referenceObject.transform.position;
+        previousObstaclePosition = referenceObject.transform.position;
+
         previosDeathTriggerPosition.y -= 2;
         previousCoinPosition.y += 1;
+        previousObstaclePosition.y += 1;
         
         startTime = Time.time;
       
@@ -41,7 +49,7 @@ public class SpawnTile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.time - startTime > timeOffset)
+        if (Time.time - startTime > timeOffset )
         {
             if (Random.value < randomValue)
                 direction = mainDirection;
@@ -54,6 +62,9 @@ public class SpawnTile : MonoBehaviour
             }
             Vector3 spawnPos = previousTilePosition + distanceBetweenTiles * direction;
             Vector3 spawnposCoin = previousCoinPosition + distanceBetweenCoin * direction;
+            //previousObstaclePosition.x = Random.Range(1, 10);
+            Vector3 spawnposObstacle = previousCoinPosition + distanceBetweenObstacle * direction;
+            //spawnposObstacle.x += Random.Range(1, 10);
             startTime = Time.time;
             Instantiate(tileToSpawn, spawnPos, Quaternion.Euler(0, 0, 0));
             previousTilePosition = spawnPos;
@@ -63,6 +74,9 @@ public class SpawnTile : MonoBehaviour
             
             Instantiate(coinToSpawn, spawnposCoin, Quaternion.Euler(0, 0, 0));
             previousCoinPosition = spawnposCoin;
+            
+            Instantiate(obstacleToSpawn, spawnposObstacle, Quaternion.Euler(0, 0, 0));
+            previousObstaclePosition = spawnposObstacle;
         }
   
         

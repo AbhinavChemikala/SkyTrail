ed4cce44e8907bcbce0d61788519919db47c85d5
Abhinav Chemikala <50442554+abhiboby@users.noreply.github.com>
2022-05-26 20:41:24 +0530

    Highscore finished

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class ScoreScript : MonoBehaviour
+{
+    public Transform player;
+    public TextMeshProUGUI scoreText;
+    private float score;
+    public TextMeshProUGUI highScoreText;
+    private float highscore = 0f;
+    void Start()
+    {
+        highscore = PlayerPrefs.GetFloat("highscore", 0);
+        highScoreText.text = "High Score - " + highscore.ToString("0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score += 0.1f;
+        scoreText.text = "Score - " + score.ToString("0");
+        if (highscore < score)
+        {
+            PlayerPrefs.SetFloat("highscore",score);
+        }
+    }
+}

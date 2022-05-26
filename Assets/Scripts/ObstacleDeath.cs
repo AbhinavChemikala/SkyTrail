b843ff0dc48ca7142bd961ca487d577f1913da9a
Abhinav Chemikala <50442554+abhiboby@users.noreply.github.com>
2022-05-26 12:19:28 +0530

    Angel Ability

--- Assets/Scripts/ObstacleDeath.cs
+++ Assets/Scripts/ObstacleDeath.cs
@@ -10,11 +10,19 @@ public class ObstacleDeath : MonoBehaviour
     
     public void OnTriggerEnter(Collider Col)
     {
-        if (Col.gameObject.tag == "Player" || Col.gameObject.tag == "Angel" || Col.gameObject.tag == "Thief" || Col.gameObject.tag == "Runner")
+        if (Col.gameObject.tag == "Angel" && AngelAbility.InvisAbil == true )
+        {
+            Debug.Log("Ability Activated");
+        }
+        else if (Col.gameObject.tag == "Player" || Col.gameObject.tag == "Angel" || Col.gameObject.tag == "Thief" || Col.gameObject.tag == "Runner")
         {
             Destroy(LevelMusic);
             SceneManager.LoadScene("Game Over");
             Destroy(Col.gameObject);
+        }
+        else
+        {
+            Debug.Log("Something wrong with Obstacle Death Script");
         }
     }
     
